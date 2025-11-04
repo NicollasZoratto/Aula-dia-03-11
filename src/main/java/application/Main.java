56e1c4c9a635afd06b41208e858a9eb27855e851
NicollasZoratto <nzcarvalho17@gmail.com>
2025-11-04 01:42:41 +0000

    Aula dia 03/11/25

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -11,6 +11,18 @@ public class Main {
         notSMS.definirPrioridade(8);
         System.out.println(notSMS.obterNivelPrioridade());
         notSMS.definirPrioridade(0);
+
+        Notificacao notif = new NotificacaoSMS(
+            "Fulano", "14991883595", "Testando", 3);
+
+        notif.exibirInformacoesRemetente();
+        notif.enviar();
+
+        Priorizavel objPriorizavel = new NotificacaoSMS(
+            "Ciclano", "1234567890", "Novo teste", 2);
+
+        objPriorizavel.definirPrioridade(6);
+
     }
 }
 
